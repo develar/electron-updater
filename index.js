b257aac64295e92f0586cb7e2726d9d5418ba0dc
Justin Chase <justin.m.chase@gmail.com>
2015-05-03 16:21:08 -0500

    remove redundant execPath variable

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var commands = require('./lib/commands.js'),
-	executor = require('./lib/executor.js'),
+	directory = require('./lib/directory.js'),
+	fs = require('fs'),
 	spawn = require('child_process').spawn
 
 console.log('loading electron updater...')
@@ -8,10 +9,9 @@ if (i > 0) {
 	var app = require('app'),
 		BrowserWindow = require('browser-window')
 
-	var arg = process.argv[i + 1]
 	var args = JSON.parse(process.argv[i + 1])
 	// {
-	// 	execPath: process.execPath,
+	//	name: appName,
 	// 	cwd: process.cwd(),
 	// 	argv: process.argv
 	// }
@@ -21,18 +21,20 @@ if (i > 0) {
 		commands.update(function (err) {
 			if(err) return console.log('' + util.inspect(err))			
 			console.log('updated!')
-			//var execName = path.basename(process.execPath)
+			var tmpDir = path.join(directory.appData(), args.appName)
+			var updateFile = path.join(tmpDir, '.update')
+			fs.unlink(updateFile, function () {
+				var execPath = args.argv.shift()
+				var child = spawn(execPath, args.argv, {
+					detached: true,
+					cwd: args.cwd,
+					stdio: [ 'ignore', 'pipe', 'pipe'] // out, err]
+				});
+				child.unref();
 
-			args.argv.shift()
-			var child = spawn(args.execPath, args.argv, {
-				detached: true,
-				cwd: args.cwd,
-				stdio: [ 'ignore', 'pipe', 'pipe'] // out, err]
-			});
-			child.unref();
-			
-			console.log('restarting app!')
-			app.quit()
+				console.log('restarting app!')
+				app.quit()
+			})
 		})
 	})
 } else {
