010b0f69219f0940da9a3a97d92b7644ac54fce8
Justin Chase <justin@evovlehq.com>
2015-05-12 14:50:59 -0500

    context tests

--- test/checkTests.js
+++ test/checkTests.js
@@ -1,232 +1,232 @@
 var proxyquire = require('proxyquire').noCallThru()
-	sinon = require('sinon'),
-	expect = require('chai').expect
+  sinon = require('sinon'),
+  expect = require('chai').expect
 
 describe('checking', function () {
 
-	var checker,
-		_download,
-		_file,
-		_fs,
-		_mocks,
-		_context
+  var checker,
+    _download,
+    _file,
+    _fs,
+    _mocks,
+    _context
 
-	beforeEach(function () {
-		_context = {
-			name: 'test-app',
-			version: '1.0.0',
-			channel: 'latest',
-			registry: 'http://npm.test.com',
-			appDir: '/test'
-		}
-		_download = {
-			getJson: sinon.stub()
-		}
-		_file = {
-			readJson: sinon.stub()
-		}
-		_fs = {
-			stat: sinon.stub()
-		}
-		_mocks = {
-			'./download.js': _download,
-			'./file.js': _file,
-			'fs': _fs
-		}
-		checker = proxyquire('../lib/check.js', _mocks)
-	})
+  beforeEach(function () {
+    _context = {
+      name: 'test-app',
+      version: '1.0.0',
+      channel: 'latest',
+      registry: 'http://npm.test.com',
+      appDir: '/test'
+    }
+    _download = {
+      getJson: sinon.stub()
+    }
+    _file = {
+      readJson: sinon.stub()
+    }
+    _fs = {
+      stat: sinon.stub()
+    }
+    _mocks = {
+      './download.js': _download,
+      './file.js': _file,
+      'fs': _fs
+    }
+    checker = proxyquire('../lib/check.js', _mocks)
+  })
 
-	describe('app', function () {
-		var _item;
-		beforeEach(function () {
-			_item = {
-				kind: 'app',
-				context: _context
-			}
-		})
+  describe('app', function () {
+    var _item;
+    beforeEach(function () {
+      _item = {
+        kind: 'app',
+        context: _context
+      }
+    })
 
-		describe('when a new version is available', function () {
-			beforeEach(function () {
-				_download.getJson
-					.withArgs('http://npm.test.com/test-app/latest')
-					.callsArgWith(1, null, {version:'1.0.1'})
-			})
-			it('should return the available version', function (done) {
-				checker.check(_item, function (err, result) {
-					if(err) return done(err)
-					expect(result.available).to.equal('1.0.1')
-					done()
-				})
-			})
-		})
+    describe('when a new version is available', function () {
+      beforeEach(function () {
+        _download.getJson
+          .withArgs('http://npm.test.com/test-app/latest')
+          .callsArgWith(1, null, {version:'1.0.1'})
+      })
+      it('should return the available version', function (done) {
+        checker.check(_item, function (err, result) {
+          if(err) return done(err)
+          expect(result.available).to.equal('1.0.1')
+          done()
+        })
+      })
+    })
 
-		describe('when a new version is not available', function () {
-			beforeEach(function () {
-				_download.getJson
-					.withArgs('http://npm.test.com/test-app/latest')
-					.callsArgWith(1, null, {version:'1.0.0'})
-			})
-			it('should return undefined', function (done) {
-				checker.check(_item, function (err, result) {
-					if(err) return done(err)
-					expect(result).to.be.undefined
-					done()
-				})
-			})
-		})
-	})
+    describe('when a new version is not available', function () {
+      beforeEach(function () {
+        _download.getJson
+          .withArgs('http://npm.test.com/test-app/latest')
+          .callsArgWith(1, null, {version:'1.0.0'})
+      })
+      it('should return undefined', function (done) {
+        checker.check(_item, function (err, result) {
+          if(err) return done(err)
+          expect(result).to.be.undefined
+          done()
+        })
+      })
+    })
+  })
 
-	describe('dependencies', function () {
+  describe('dependencies', function () {
 
-		var _item;
-		beforeEach(function () {
-			_context.dependencies = {
-				'test-dependency': '^1.0.0'
-			}
-			_item = {
-				kind: 'dependencies',
-				context: _context
-			}
-		})
+    var _item;
+    beforeEach(function () {
+      _context.dependencies = {
+        'test-dependency': '^1.0.0'
+      }
+      _item = {
+        kind: 'dependencies',
+        context: _context
+      }
+    })
 
-		describe('when the dependency doesnt exist', function () {
-			beforeEach(function () {
-				_file.readJson
-					.callsArgWith(1, 'ENOENT')
-				_download.getJson
-					.withArgs('http://npm.test.com/test-dependency/^1.0.0')
-					.callsArgWith(1, null, {version:'1.0.0'})
-			})
-			it('should return latest version', function (done) {
-				checker.check(_item, function (err, result) {
-					if(err) return done(err)
-					expect(result[0].available).to.equal('1.0.0')
-					done()
-				})
-			})
-		})
+    describe('when the dependency doesnt exist', function () {
+      beforeEach(function () {
+        _file.readJson
+          .callsArgWith(1, 'ENOENT')
+        _download.getJson
+          .withArgs('http://npm.test.com/test-dependency/^1.0.0')
+          .callsArgWith(1, null, {version:'1.0.0'})
+      })
+      it('should return latest version', function (done) {
+        checker.check(_item, function (err, result) {
+          if(err) return done(err)
+          expect(result[0].available).to.equal('1.0.0')
+          done()
+        })
+      })
+    })
 
-		describe('when a new version is available', function () {
-			beforeEach(function () {
-				_file.readJson
-					.callsArgWith(1, null, {version:'1.0.0'})
-				_download.getJson
-					.withArgs('http://npm.test.com/test-dependency/^1.0.0')
-					.callsArgWith(1, null, {version:'1.0.1'})
-			})
-			it('should return the newer version', function (done) {
-				checker.check(_item, function (err, result) {
-					if(err) return done(err)
-					expect(result[0].available).to.equal('1.0.1')
-					done()
-				})
-			})
-		})
+    describe('when a new version is available', function () {
+      beforeEach(function () {
+        _file.readJson
+          .callsArgWith(1, null, {version:'1.0.0'})
+        _download.getJson
+          .withArgs('http://npm.test.com/test-dependency/^1.0.0')
+          .callsArgWith(1, null, {version:'1.0.1'})
+      })
+      it('should return the newer version', function (done) {
+        checker.check(_item, function (err, result) {
+          if(err) return done(err)
+          expect(result[0].available).to.equal('1.0.1')
+          done()
+        })
+      })
+    })
 
-		describe('when a new version is not available', function () {
-			beforeEach(function () {
-				_file.readJson
-					.callsArgWith(1, null, {version:'1.0.0'})
-				_download.getJson
-					.withArgs('http://npm.test.com/test-dependency/^1.0.0')
-					.callsArgWith(1, null, {version:'1.0.0'})
-			})
-			it('should return undefined', function (done) {
-				checker.check(_item, function (err, result) {
-					if(err) return done(err)
-					expect(result[0]).to.be.undefined
-					done()
-				})
-			})
-		})
-	})
+    describe('when a new version is not available', function () {
+      beforeEach(function () {
+        _file.readJson
+          .callsArgWith(1, null, {version:'1.0.0'})
+        _download.getJson
+          .withArgs('http://npm.test.com/test-dependency/^1.0.0')
+          .callsArgWith(1, null, {version:'1.0.0'})
+      })
+      it('should return undefined', function (done) {
+        checker.check(_item, function (err, result) {
+          if(err) return done(err)
+          expect(result[0]).to.be.undefined
+          done()
+        })
+      })
+    })
+  })
 
-	describe('plugins', function () {
-		var _item;
-		beforeEach(function () {
-			_context.plugins = {
-				'test-plugin': '^1.0.0'
-			}
-			_item = {
-				kind: 'plugins',
-				context: _context
-			}
-		})
+  describe('plugins', function () {
+    var _item;
+    beforeEach(function () {
+      _context.plugins = {
+        'test-plugin': '^1.0.0'
+      }
+      _item = {
+        kind: 'plugins',
+        context: _context
+      }
+    })
 
-		describe('when .current file is missing', function () {
-			beforeEach(function () {
-				_file.readJson
-					.callsArgWith(1, 'ENOENT')
-				_fs.stat
-					.callsArgWith(1, 'ENOENT')
-				_download.getJson
-					.withArgs('http://npm.test.com/test-plugin/^1.0.0')
-					.callsArgWith(1, null, {version:'1.0.0'})
-			})
-			it('should return the latest version', function (done) {
-				checker.check(_item, function (err, result) {
-					if(err) return done(err)
-					expect(result[0].available).to.equal('1.0.0')
-					done()
-				})
-			})
-		})
+    describe('when .current file is missing', function () {
+      beforeEach(function () {
+        _file.readJson
+          .callsArgWith(1, 'ENOENT')
+        _fs.stat
+          .callsArgWith(1, 'ENOENT')
+        _download.getJson
+          .withArgs('http://npm.test.com/test-plugin/^1.0.0')
+          .callsArgWith(1, null, {version:'1.0.0'})
+      })
+      it('should return the latest version', function (done) {
+        checker.check(_item, function (err, result) {
+          if(err) return done(err)
+          expect(result[0].available).to.equal('1.0.0')
+          done()
+        })
+      })
+    })
 
-		describe('when plugin is missing', function () {
-			beforeEach(function () {
-				_file.readJson
-					.callsArgWith(1, null, {'test-plugin': '1.0.0'})
-				_fs.stat
-					.callsArgWith(1, 'ENOENT')
-				_download.getJson
-					.withArgs('http://npm.test.com/test-plugin/^1.0.0')
-					.callsArgWith(1, null, {version:'1.0.0'})
-			})
-			it('should return the latest version', function (done) {
-				checker.check(_item, function (err, result) {
-					if(err) return done(err)
-					expect(result[0].available).to.equal('1.0.0')
-					done()
-				})
-			})
-		})
+    describe('when plugin is missing', function () {
+      beforeEach(function () {
+        _file.readJson
+          .callsArgWith(1, null, {'test-plugin': '1.0.0'})
+        _fs.stat
+          .callsArgWith(1, 'ENOENT')
+        _download.getJson
+          .withArgs('http://npm.test.com/test-plugin/^1.0.0')
+          .callsArgWith(1, null, {version:'1.0.0'})
+      })
+      it('should return the latest version', function (done) {
+        checker.check(_item, function (err, result) {
+          if(err) return done(err)
+          expect(result[0].available).to.equal('1.0.0')
+          done()
+        })
+      })
+    })
 
-		describe('when a new version is available', function () {
-			beforeEach(function () {
-				_file.readJson
-					.callsArgWith(1, null, {'test-plugin': '1.0.0'})
-				_fs.stat
-					.callsArgWith(1, null)
-				_download.getJson
-					.withArgs('http://npm.test.com/test-plugin/^1.0.0')
-					.callsArgWith(1, null, {version:'1.0.1'})
-			})
-			it('should return the latest version', function (done) {
-				checker.check(_item, function (err, result) {
-					if(err) return done(err)
-					expect(result[0].available).to.equal('1.0.1')
-					done()
-				})
-			})
-		})
+    describe('when a new version is available', function () {
+      beforeEach(function () {
+        _file.readJson
+          .callsArgWith(1, null, {'test-plugin': '1.0.0'})
+        _fs.stat
+          .callsArgWith(1, null)
+        _download.getJson
+          .withArgs('http://npm.test.com/test-plugin/^1.0.0')
+          .callsArgWith(1, null, {version:'1.0.1'})
+      })
+      it('should return the latest version', function (done) {
+        checker.check(_item, function (err, result) {
+          if(err) return done(err)
+          expect(result[0].available).to.equal('1.0.1')
+          done()
+        })
+      })
+    })
 
-		describe('when a new version is not available', function () {
-			beforeEach(function () {
-				_file.readJson
-					.callsArgWith(1, null, {'test-plugin': '1.0.0'})
-				_fs.stat
-					.callsArgWith(1, null)
-				_download.getJson
-					.withArgs('http://npm.test.com/test-plugin/^1.0.0')
-					.callsArgWith(1, null, {version:'1.0.0'})
-			})
-			it('should return undefined', function (done) {
-				checker.check(_item, function (err, result) {
-					if(err) return done(err)
-					expect(result[0]).to.be.undefined
-					done()
-				})
-			})
-		})
-	})
+    describe('when a new version is not available', function () {
+      beforeEach(function () {
+        _file.readJson
+          .callsArgWith(1, null, {'test-plugin': '1.0.0'})
+        _fs.stat
+          .callsArgWith(1, null)
+        _download.getJson
+          .withArgs('http://npm.test.com/test-plugin/^1.0.0')
+          .callsArgWith(1, null, {version:'1.0.0'})
+      })
+      it('should return undefined', function (done) {
+        checker.check(_item, function (err, result) {
+          if(err) return done(err)
+          expect(result[0]).to.be.undefined
+          done()
+        })
+      })
+    })
+  })
 })
---
+++ test/contextTests.js
@@ -0,0 +1,166 @@
+var proxyquire = require('proxyquire').noCallThru()
+  sinon = require('sinon'),
+  expect = require('chai').expect
+
+describe('context', function () {
+
+  var context,
+    _file,
+    _fs,
+    _mocks,
+    _context
+
+  beforeEach(function () {
+    _file = {
+      readJson: sinon.stub()
+    }
+    _fs = {
+      stat: sinon.stub(),
+      readFile: sinon.stub(),
+      readdir: sinon.stub()
+    }
+    _mocks = {
+      './file.js': _file,
+      'fs': _fs
+    }
+    context = proxyquire('../lib/context.js', _mocks)
+  })
+
+  describe('load', function () {
+    beforeEach(function () {
+      _fs.readdir
+        .callsArgWith(1, 'ENOENT')
+      _fs.readFile
+        .callsArgWith(1, 'ENOENT')
+      _fs.stat
+        .callsArgWith(1, 'ENOENT')
+      _file.readJson
+        .callsArgWith(1, null, {name:'test'})
+    })
+    describe('dev repo', function () {
+      ['.git', '.svn'].forEach(function (devDir) {
+        describe('when in a ' + devDir + ' repo', function () {
+          it('should set dev: true in context', function (done) {           
+            _fs.readdir
+              .withArgs(path.join('/test', devDir))
+              .callsArgWith(1, null, {})
+            context.load('/test', function (err, context) {
+              if(err) return done(err)
+              expect(context.dev).to.be.true
+              done()  
+            })
+          })
+        })
+      })
+    })
+    describe('channel', function () {
+      describe('when .channel is available', function () {
+        it('should set the channel to file contents', function (done) {
+          _fs.readFile.callsArgWith(1, null, 'beta')
+          context.load('/test', function (err, context) {
+            if(err) return done(err)
+            expect(context.channel).to.equal('beta')
+            done()
+          })
+        })
+      })
+      describe('when the .channel is not available', function () {
+        it('should default the channel to latest', function (done) {
+          context.load('/test', function (err, context) {
+            if(err) return done(err)
+            expect(context.channel).to.equal('latest')
+            done()
+          })
+        })
+      })
+    })
+    describe('pending update', function () {
+      describe('when the .update file is available', function () {
+        it('should set pendingUpdate to true', function (done) {
+          _fs.stat.callsArgWith(1, null, {isFile: sinon.stub().returns(true) })
+          context.load('/test', function (err, context) {
+            if(err) return done(err)
+            expect(context.pendingUpdate).to.be.true
+            done()
+          })
+        })
+      })
+      describe('when the .update file is not available', function () {
+        it('should set pendingUpdate to false', function (done) {
+          context.load('/test', function (err, context) {
+            if(err) return done(err)
+            expect(context.pendingUpdate).to.be.false
+            done()
+          })
+        })
+      })
+    })
+    describe('dependencies', function () {
+      describe('when the package has dependencies', function () {
+        it('should set dependencies to package dependencies', function (done) {
+          var deps = {}
+          _file.readJson.callsArgWith(1, null, {name: 'test', dependencies: deps})
+          context.load('/test', function (err, context) {
+            if(err) return done(err)
+            expect(context.dependencies).to.equal(deps)
+            done()
+          })
+        })  
+      })
+      describe('when the package doesnt have dependencies', function () {
+        it('should default dependencies to {}', function (done) {
+          context.load('/test', function (err, context) {
+            if(err) return done(err)
+            expect(context.dependencies).to.deep.equal({})
+            done()
+          })
+        })
+      })
+    })
+    describe('plugins', function () {
+      describe('when the package has plugins', function () {
+        it('should set the plugins to package plugins', function (done) {
+          var plugins = {}
+          _file.readJson.callsArgWith(1, null, {name: 'test', plugins: plugins})
+          context.load('/test', function (err, context) {
+            if(err) return done(err)
+            expect(context.plugins).to.equal(plugins)
+            done()
+          })
+        })
+      })
+      describe('when the package has no plugins', function () {
+        it('should default to {}', function (done) {
+          context.load('/test', function (err, context) {
+            if(err) return done(err)
+            expect(context.plugins).to.deep.equal({})
+            done()
+          })
+        })
+      })
+    })
+    it('should set name to the package name', function (done) {
+      context.load('/test', function (err, context) {
+        if(err) return done(err)
+        expect(context.name).to.equal('test')
+        done()
+      })
+    })
+    it('should set version to the package version', function (done) {
+      _file.readJson.callsArgWith(1, null, {name: 'test', version: '1.0.0'})
+      context.load('/test', function (err, context) {
+        if(err) return done(err)
+        expect(context.version).to.equal('1.0.0')
+        done()
+      })
+    })
+    it('should set appDir to the provided appDir', function (done) {
+      context.load('/test', function (err, context) {
+        if(err) return done(err)
+        expect(context.appDir).to.equal('/test')
+        done()
+      })
+    })    
+    it('should resolve the registry from .npmrc settings')
+  })
+})
