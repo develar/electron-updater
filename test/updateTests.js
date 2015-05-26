ce4c585d7d18715aa6d379613db77865111f6ebe
Justin Chase <justin@evovlehq.com>
2015-05-26 11:25:42 -0500

    update tests for plugins

--- test/updateTests.js
+++ test/updateTests.js
@@ -9,25 +9,53 @@ describe('update', function () {
   var update,
     _mocks,
     _deps,
-    _download
+    _download,
+    _res,
+    _unpack,
+    _file,
+    _directory
 
   beforeEach(function () {
     _deps = {
         context: { 
+            name: 'test',
             appDir: '/test'
         },
         dependencies: [],
         plugins: []
     }
     _download = {
-        getJson: sinon.stub()
+        getJson: sinon.stub(),
+        get: sinon.stub()
+    }
+    _res = {
+    }
+    _unpack = {
+        extract: sinon.stub()
+    }
+    _file = {
+        readJson: sinon.stub(),
+        writeJson: sinon.stub()
+    }
+    _directory = {
+        create: sinon.stub(),
+        remove: sinon.stub()
     }
     _mocks = {
-        './download.js': _download
+        './download.js': _download,
+        './unpack.js': _unpack,
+        './file.js': _file,
+        './directory.js': _directory
     }
     update = proxyquire('../lib/update.js', _mocks)
 
     _download.getJson.callsArgWith(1, null, { dist: { tarball: 'http://test.com/update.tgz' } })
+    _download.get.callsArgWith(1, null, _res)
+    _unpack.extract.callsArgWith(2, null)
+    _file.readJson.callsArgWith(1, null, { dependencies: {} })
+    _file.writeJson.callsArgWith(2, null)
+    _directory.create.callsArgWith(1, null)
+    _directory.remove.callsArgWith(1, null)
   })
 
   it('should update dependencies')
@@ -59,16 +87,59 @@ describe('update', function () {
     })
     it('should update dependencies', function (done) {
         update.update(_deps, function (err) {
-            expect(_download.getJson.called).to.be.false
+            expect(_download.getJson.called).to.be.true
+            expect(_unpack.extract.called).to.be.true
             done(err)
         })        
+    })
+    describe('with sub-dependencies', function () {
+        beforeEach(function () {
+            var pkg = { dependencies: { testSub: '^1.0.0' } }
+            _file.readJson.onFirstCall().callsArgWith(1, null, pkg)
+        })
+        it('should update sub dependencies', function (done) {            
+            update.update(_deps, function (err) {
+                expect(_download.getJson.calledTwice).to.be.true
+                expect(_unpack.extract.calledTwice).to.be.true
+                done(err)
+            })
+        })
     })
   })
 
   describe('plugins', function () {
-    describe('in dev environment', function () {
-        it('should update plugins')
+    beforeEach(function () {
+        _deps.plugins.push({ name: 'test', available: '1.0.1' })
     })
-    it('should update plugins')
+    describe('in dev environment', function () {
+        it('should update plugins', function (done) {
+            update.update(_deps, function (err) {
+                expect(_download.getJson.called).to.be.true
+                expect(_unpack.extract.called).to.be.true
+                done(err)
+            })
+        })
+    })    
+    describe('with sub-dependencies', function () {
+        beforeEach(function () {
+            var pkg = { dependencies: { testSub: '^1.0.0' } }
+            _file.readJson.onFirstCall().callsArgWith(1, null, pkg)
+        })
+        it('should update sub dependencies', function (done) {            
+            update.update(_deps, function (err) {
+                expect(_download.getJson.calledTwice).to.be.true
+                expect(_unpack.extract.calledTwice).to.be.true
+                done(err)
+            })
+        })
+    })
+
+    it('should update plugins', function (done) {
+        update.update(_deps, function (err) {
+            expect(_download.getJson.called).to.be.true
+            expect(_unpack.extract.called).to.be.true
+            done(err)
+        })
+    })
   })
 })
