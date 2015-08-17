09aa79ca86087dd77d59bd965b0d0f71dbe87635
Justin Chase <justin@evovlehq.com>
2015-08-17 13:29:38 -0500

    cache tests

--- test/cacheTests.js
+++ test/cacheTests.js
@@ -1,9 +1,10 @@
 var proxyquire = require('proxyquire').noCallThru()
     sinon = require('sinon'),
-    expect = require('chai').expect;
+    expect = require('chai').expect,
+    stream = require('./mockStream');
 
 describe('caching,', function () {
-
+  var url = 'http://example.com/test';
   var cache,
     _got,
     _fs,
@@ -18,21 +19,25 @@ describe('caching,', function () {
       name: 'test-app',
       publisher: 'Testers'
     };
-    _got = sinon.stub();
+    _got = {
+      stream: sinon.stub().returns(new stream.MockResponseStream('test', 'test'))
+    };
     _fs = {
       readFile: sinon.stub().callsArgWith(1, 'test error.'),
-      createReadStream: sinon.stub(),
-      createWriteStream: sinon.stub()
+      createReadStream: sinon.stub().returns(new stream.MockReadable('test')),
+      createWriteStream: sinon.stub().returns(new stream.MockWritable())
     };
     _unpack = {
       extract: sinon.stub().callsArgWith(3, null)
     };
     _directory = {
-      create: sinon.stub().callsArgWith(1, null)
-    }
+      create: sinon.stub().callsArgWith(1, null),
+      appDir: sinon.stub().returns('/temp')
+    };
     _logger = {
-      log: sinon.stub()
-    }
+      log: sinon.stub(),
+      error: sinon.stub()
+    };
     _mocks = {
       'fs': _fs,
       'got': _got,
@@ -43,46 +48,86 @@ describe('caching,', function () {
   })
 
   describe('when chached file doesnt exist,', function () {
-
-    it('should download desired file', function () {
-      assert(false)
+    it('should download desired file', function (done) {
+      _fs.readFile.onSecondCall().callsArgWith(1, null, 'test');
+      _got.stream.onSecondCall().returns(new stream.MockResponseStream('test', ''));
+      cache.get(url, '/test', null, _context, _logger, function (err) {        
+        expect(_got.stream.called).to.be.true
+        done(err)
+      })
     })
-
   })
 
   describe('when cached file already exists,', function () {
 
     describe('and an expected hash is provided,', function () {
 
+      var expectedHash = 'a94a8fe5ccb19ba61c4c0873d391e987982fbbd3'; // sha1('test')
+
       it('should download if the hash doesnt match', function () {
-        assert(false)
+        // File contains bad content
+        _fs.readFile.callsArgWith(1, null, 'test123');
+        _got.stream.returns(new stream.MockResponseStream(null, 'test')); // file content should be test
+
+        // After download, content is correct.
+        _fs.readFile.onSecondCall().callsArgWith(1, null, 'test');
+
+        cache.get(url, '/test', expectedHash, _context, _logger, function (err) {
+          expect(_got.stream().readData).to.be.true;
+          done(err)
+        })
       })
 
-      it('should extract if the hash does match', function () {
-        assert(false)
+      it('should extract if the hash matches', function (done) {
+        // File contains correct data
+        _fs.readFile.callsArgWith(1, null, 'test');
+        cache.get(url, '/test', expectedHash, _context, _logger, function (err) {
+          expect(_unpack.extract.called).to.be.true;
+          done(err)
+        })
       })
-
     })
 
     describe('and an expected hash is null,', function () {
 
-      it('should download headers', function () {
-        assert(false)
-      })
-
       describe('and hash in headers matches', function () {
-        it('should extract', function () {
-          assert(false)
+
+        beforeEach(function () {
+            // local is good
+            _fs.readFile.callsArgWith(1, null, 'test');
+            _got.stream.returns(new stream.MockResponseStream('test', ''));          
+        })
+
+        it('should download headers only', function (done) {
+            cache.get(url, '/test', null, _context, _logger, function (err) {
+              expect(_got.stream().readData).to.be.false;
+              done(err)
+            })
+        })
+
+        it('should extract', function (done) {
+          cache.get(url, '/test', null, _context, _logger, function (err) {
+            expect(_unpack.extract.called).to.be.true;
+            done(err)
+          })
         })
       })
       describe('and hash in headers doesnt match', function () {
-        it('should download', function () {
-          assert(false)
+        it('should download', function (done) {
+          // local file is bad 
+          _fs.readFile.callsArgWith(1, null, 'test');
+          _got.stream.returns(new stream.MockResponseStream('test123', '')); // file content should be test123
+
+          // after download file should contain test123
+          _fs.readFile.onSecondCall().callsArgWith(1, null, 'test123');
+          _got.stream.onSecondCall().returns(new stream.MockResponseStream('test123', '')); // now they match
+
+          cache.get(url, '/test', null, _context, _logger, function (err) {
+            expect(_got.stream.called).to.be.true;
+            done(err)
+          })
         })
       })
-
     })
-
   })
-
 })
---
+++ test/mockStream.js
@@ -0,0 +1,74 @@
+var Readable = require('stream').Readable,
+    Writable = require('stream').Writable,
+    util = require('util'),
+    sinon = require('sinon'),
+    crypto = require('crypto');
+
+function md5(input) {
+  var alg = crypto.createHash('md5')
+  alg.update(input)
+  return alg.digest('hex');
+}
+
+function MockReadable(data) {
+  Readable.call(this, {});
+  this._data = data;
+}
+
+function MockWritable() {
+  Writable.call(this, {});
+  this._data = null;
+}
+
+function MockResponseStream(etag, data) {
+  Readable.call(this, {});
+  console.log('creating MockResponseStream')
+  this._etag = etag ? md5(etag) : null;
+  this._data = data;
+  this.readData = false;
+
+  this._on = this.on;
+  this.on = _on;
+}
+
+function _read() {
+  console.log('reading...');
+  var buf = new Buffer(this._data, 'utf8');
+  this.push(buf);
+  this.push(null);
+  this.readData = true;
+}
+
+function _write(chunk, encoding, callback) {
+  console.log('writing...');
+  this._data = chunk;
+  callback();
+}
+
+function _on(type, callback) {
+  console.log('on: ' + type)
+  if(type == 'response') {
+    callback({
+      headers: { etag: `"${this._etag}"` }
+    })
+  } else {
+    this._on(type, callback);
+  }
+}
+
+
+util.inherits(MockReadable, Readable);
+MockReadable.prototype._read = _read;
+
+util.inherits(MockWritable, Writable);
+MockWritable.prototype._write = _write;
+
+util.inherits(MockResponseStream, Readable);
+MockResponseStream.prototype._read = _read;
+MockResponseStream.prototype.end = sinon.stub();
+
+module.exports = {
+  MockReadable: MockReadable,
+  MockWritable: MockWritable,
+  MockResponseStream: MockResponseStream
+};
